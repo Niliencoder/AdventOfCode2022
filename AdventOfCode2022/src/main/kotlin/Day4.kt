dc23ef90558b94f4590655d16f8969ac9aee65ca
Niliencoder <niliencoder@protonmail.com>
2022-12-04 13:48:09 +0100

    Day 4 removed set and used any for part 2

--- AdventOfCode2022/src/main/kotlin/Day4.kt
+++ AdventOfCode2022/src/main/kotlin/Day4.kt
@@ -3,7 +3,6 @@ import kotlin.io.path.Path
 
 fun main() {
     val input: List<String> = Files.readAllLines(Path("src/main/resources/input4.txt"))
-
     var sum1 = 0
     var sum2 = 0
 
@@ -11,21 +10,20 @@ fun main() {
         val splitted = i.split(",")
         val lower1 = splitted[0].split("-")[0].toInt()
         val upper1 = splitted[0].split("-")[1].toInt()
-
         val lower2 = splitted[1].split("-")[0].toInt()
         val upper2 = splitted[1].split("-")[1].toInt()
 
+        //Part 1
         if(lower1 <= lower2 && upper1 >= upper2 || lower1 >= lower2 && upper1 <= upper2){
             sum1++
         }
 
+        //Part 2
         val assignments1: List<Int> = (lower1..upper1).toList()
         val assignments2: List<Int> = (lower2..upper2).toList()
-        val intersections = assignments1.intersect(assignments2.toSet())
-        if(intersections.isNotEmpty()){
+        if(assignments1.any{assignments2.contains(it)}){
             sum2++
         }
-
     }
 
     println(sum1)
